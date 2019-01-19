7697ef7cbad6b143346e0801223a5b76c117b3ed
Emily Wagner <emilywagner598@gmail.com>
2019-01-19 00:03:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,3 +29,15 @@ end
 def using_insert (array, element)
   array.insert(4,element)
 end
+
+def using_uniq (array)
+  array.uniq
+end
+
+def using_flatten (array)
+  array.flatten
+end
+
+def using_delete_at (array, string)
+  array.delete(string)
+end
